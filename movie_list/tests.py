3d4e1c782ceb1345e0f7c228f1e107b61a55532c
Shohruhbek <rahmonberdiyevshohruhbek1@gmail.com>
2023-07-20 20:08:51 +0900

    Update tests.py

--- movie_list/tests.py
+++ movie_list/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from django.test.client import Client
 # Create your tests here.
+class HomeViewTest(TestCase):
+    def test_index(self):
+        client= Client()
+        response = client.get('/')
+        assert response.status_code == 200
+
